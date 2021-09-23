7e14379263e08c7fe129a1e5bce7166eb461044f
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-09-23 11:19:45 +0900

    a

---
+++ app/src/main/java/jp/techacademy/ayumi/sonia/Searchmovie.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.ayumi.sonia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Searchmovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchmovie)
+    }
+}
---
+++ app/src/main/java/jp/techacademy/ayumi/sonia/watchmovie.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.ayumi.sonia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class watchmovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_watchmovie)
+    }
+}
