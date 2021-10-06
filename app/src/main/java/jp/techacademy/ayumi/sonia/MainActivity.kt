891a02be390a1a3a968d3e2f164d43a828245dbe
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-10-06 21:06:30 +0900

    a

--- app/src/main/java/jp/techacademy/ayumi/sonia/MainActivity.kt
+++ app/src/main/java/jp/techacademy/ayumi/sonia/MainActivity.kt
@@ -4,14 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intent = Intent(applicationContext, Searchmovie::class.java)
-        startActivity(intent)
-
+        Timer().schedule(3000) {
+            val intent = Intent(applicationContext, Searchmovie::class.java)
+            startActivity(intent)
+        }
     }
 }
